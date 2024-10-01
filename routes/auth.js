3119d39c450b307fec05ae00742763bda1daaa0b
Mansour Bashabsheh <142018389+mansourelbash@users.noreply.github.com>
2024-10-01 16:42:46 +0300

    add evrthings

--- routes/auth.js
+++ routes/auth.js
@@ -47,4 +47,11 @@ router.post('/login', async (req, res) => {
   }
 });
 
-module.exports = router;
+router.get('/users', async (req, res) => {
+    try {
+      const users = await User.find({}, '-password'); // Exclude the password field from the response
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
