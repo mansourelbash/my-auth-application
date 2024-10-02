665c4535c09728f6e4b402153e3fda7ff52f28b7
Mansour Bashabsheh <142018389+mansourelbash@users.noreply.github.com>
2024-10-02 16:56:33 +0300

    last changes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./swaggerOptions'); // No need for .js extension
 
 dotenv.config();
 connectDB();
@@ -10,15 +12,19 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+const allowedOrigins = [
+  'http://localhost:5731',
+  'https://my-auth-application.onrender.com'
+];
+
 app.use(cors({
-  origin: 'https://my-auth-application.onrender.com' // replace with your actual domain
+  origin: allowedOrigins,
 }));
 
 app.use('/api/auth', authRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
